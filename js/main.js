b26126dc21905c183c9be937eb1da3dc39d60e5a
Mathieu Letendre-Jauniaux <MathieuLJ@Gmail.com>
2017-02-26 07:56:33 -0500

    First commit. Set up starting point.

---
+++ js/main.js
@@ -0,0 +1,15 @@
+/**
+ * Created by mletendre on 2/25/17.
+ */
+
+(function () {
+    'use strict';
+    const main = new Vue({
+        el: "#main-mount",
+        template: "<h1>{{message}}</h1>",
+        data: {
+            message: "test"
+        }
+    })
+
+})();
